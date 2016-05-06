31addda406515bf78d121b66c23dbf030d5b9129
Jim Nelson <jim@midnightfreddie.com>
2016-05-06 12:29:47 -0500

    Zalgo He Comes

---
+++ zalgo/readme.md
@@ -0,0 +1,1 @@
+Code in this directory is from https://github.com/jorgechamorro/zalgo.js which is a fork/update to https://github.com/Marak/zalgo.js .
---
+++ zalgo/zalgo.js
@@ -0,0 +1,71 @@
+//Original by Marak Squires, five years ago... :-)
+//2015-03-24 jorge@jorgechamorro.com
+//node zalgo.js 'The text' up down mid
+
+var soul = {
+  "up" : [
+    '̍','̎','̄','̅',
+    '̿','̑','̆','̐',
+    '͒','͗','͑','̇',
+    '̈','̊','͂','̓',
+    '̈','͊','͋','͌',
+    '̃','̂','̌','͐',
+    '̀','́','̋','̏',
+    '̒','̓','̔','̽',
+    '̉','ͣ','ͤ','ͥ',
+    'ͦ','ͧ','ͨ','ͩ',
+    'ͪ','ͫ','ͬ','ͭ',
+    'ͮ','ͯ','̾','͛',
+    '͆','̚'
+    ],
+  "down" : [
+    '̖','̗','̘','̙',
+    '̜','̝','̞','̟',
+    '̠','̤','̥','̦',
+    '̩','̪','̫','̬',
+    '̭','̮','̯','̰',
+    '̱','̲','̳','̹',
+    '̺','̻','̼','ͅ',
+    '͇','͈','͉','͍',
+    '͎','͓','͔','͕',
+    '͖','͙','͚','̣'
+    ],
+  "mid" : [
+    '̕','̛','̀','́',
+    '͘','̡','̢','̧',
+    '̨','̴','̵','̶',
+    '͜','͝','͞',
+    '͟','͠','͢','̸',
+    '̷','͡',' ҉'
+    ]
+};
+
+var all = [].concat(soul.up, soul.down, soul.mid);
+
+Array.prototype.times= function (f) {
+  while (this[0]) f(this[0]), --this[0];
+};
+
+function rnd (n) { return Math.floor(Math.random()*n) }
+
+function is_char (character) {
+  var bool = false;
+  all.filter(function (i) { bool= (i === character) });
+  return bool;
+}
+
+function zalgo (text, up, down, mid) {
+	var result= '';
+	text.split('').forEach(function (v,i,o) {
+		if (!is_char(i)) {
+			result+= v;
+			var counts= {up:rnd(up), down:rnd(down), mid:rnd(mid)};
+			Object.getOwnPropertyNames(counts).forEach(function (v,i,o) {
+				[counts[v]].times(function () {
+				  result+= soul[v][rnd(soul[v].length)];
+				});
+			});
+		}
+	});
+  return result;
+}
